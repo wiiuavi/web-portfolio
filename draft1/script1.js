06b5b608580133a9deac618b12be8dfbee59517d
wiiuavi <kumar2008avi@gmail.com>
2026-06-16 10:47:29 +0100

    added drafts, possible ideas ect

---
+++ draft1/script1.js
@@ -0,0 +1,4 @@
+function showPage(pageId) {
+    document.querySelectorAll('.page').forEach(p => p.style.display = 'none')
+    document.getElementById(pageId).style.display = 'block'
+}
---
+++ draft2/script.js
@@ -0,0 +1,10 @@
+// waitinf for html/js to connect
+document.addEventListener('DOMContentLoaded', () => {
+        const actionBtn = document.getElementById('action-btn');
+
+            // e.g. button
+                actionBtn.addEventListener('click', () => {
+                            alert('hi :|');
+                });
+            });
+            
